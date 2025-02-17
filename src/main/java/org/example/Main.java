18b5535ccdc00ebb65d04e32d1b7ea7cd8a633c4
Thiago1722 <thiagosbthiago@gmail.com>
2025-02-17 15:49:03 -0300

    refactor: aicionando atualizacao nova do mediator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,77 @@
-package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+interface ChatMediator {
+    void sendMessage(String message, User user);
+    void addUser(User user);
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class ChatMediatorImpl implements ChatMediator {
+    private List<User> users;
+
+    public ChatMediatorImpl() {
+        this.users = new ArrayList<>();
+    }
+
+    @Override
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    @Override
+    public void sendMessage(String message, User user) {
+        for (User u : users) {
+            if (!u.equals(user)) {
+                u.receive(message);
+            }
         }
+    }
+}
+
+abstract class User {
+    protected ChatMediator mediator;
+    protected String name;
+
+    public User(ChatMediator mediator, String name) {
+        this.mediator = mediator;
+        this.name = name;
+    }
+
+    public abstract void send(String message);
+    public abstract void receive(String message);
+}
+
+class ConcreteUser extends User {
+
+    public ConcreteUser(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void send(String message) {
+        System.out.println(this.name + " enviando mensagem: " + message);
+        mediator.sendMessage(message, this);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println(this.name + " recebeu mensagem: " + message);
+    }
+}
+
+    class ChatApplication {
+    public static void main(String[] args) {
+        ChatMediator chatMediator = new ChatMediatorImpl();
+
+        User user1 = new ConcreteUser(chatMediator, "RENATO");
+        User user2 = new ConcreteUser(chatMediator, "MIGUEL");
+        User user3 = new ConcreteUser(chatMediator, "COSME");
+
+        chatMediator.addUser(user1);
+        chatMediator.addUser(user2);
+        chatMediator.addUser(user3);
+
+        user1.send("Olá pessoal!");
+        user2.send("Oi RENATO!");
     }
 }
